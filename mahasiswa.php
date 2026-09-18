4ccf690585de45e0b1c9ff30c52e8fc446812643
unknown <251150074@mahasiswa.atamaluhur.ac.id>
2026-09-18 18:25:59 +0700

    first commit

---
+++ mahasiswa.php
@@ -0,0 +1,13 @@
+{
+  "status": "success"
+  "massege": "Data mahasiswa berhasil diambil",
+  "data": {
+    "id": 1,
+    "nim": "251150074",
+    "nama": "PANJI",
+    "email": "budi@example.com",
+    "jurusan": "Teknik Informatika",
+    "semester": 5,
+    "ipk": 3.75
+  }
+}
